1546cddbbb935d91605ac39811eeb245ad906acf
Raoul1808 <raoulthegeek@gmail.com>
2024-11-21 13:52:18 +0100

    Update dapui events

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -13,22 +13,51 @@ dap.configurations.cpp = {
 		program = function()
 			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
 		end,
+		request = "launch",
 		MIMode = "gdb",
 		miDebuggerPath = "/usr/bin/gdb",
 		cwd = "${workspaceFolder}",
+		setupCommands = {
+			{
+				text = '-enable-pretty-printing',
+				description = 'enable pretty printing',
+				ignoreFailures = false,
+			},
+		},
 	}
 }
 dap.configurations.c = dap.configurations.cpp
 
+local open_dapui = function()
+	dapui.open();
+	vim.cmd("NvimTreeFresh")
+end
+
+local close_dapui = function()
+	dapui.close();
+	vim.cmd("NvimTreeFresh")
+end
+
+vim.api.nvim_create_user_command(
+	"ToggleDapUi",
+	function()
+		require("dapui").toggle()
+		vim.cmd("NvimTreeRefresh")
+	end,
+	{}
+)
+
 dap.listeners.before.attach.dapui_config = function()
-	dapui.open()
+	open_dapui()
 end
 dap.listeners.before.launch.dapui_config = function()
-	dapui.open()
+	open_dapui()
 end
 dap.listeners.before.event_terminated.dapui_config = function()
-	dapui.close()
+	close_dapui()
 end
 dap.listeners.before.event_exited.dapui_config = function()
-	dapui.close()
+	close_dapui()
 end
+
+dapui.setup({})
