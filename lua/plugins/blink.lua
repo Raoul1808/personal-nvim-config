ed6d63b48a6795c5463b36e82e06e8a80eba385c
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-02-04 18:32:44 +0100

    Add blink cmp

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,18 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "1.*",
+    opts = {
+        keymap = { preset = "super-tab" },
+        appearance = {
+            nerd_font_variant = "normal",
+        },
+        completion = {
+            documentation = { auto_show = true },
+        },
+        sources = {
+            default = { 'lsp', 'path', 'snippets' },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+}
