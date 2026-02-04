19cc65051fd67a636049420efd90405d32d3f883
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-02-04 17:54:06 +0100

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {"vim", "regex", "lua", "bash", "markdown", "markdown_inline"},
+            auto_install = true,
+        })
+    end,
+}
