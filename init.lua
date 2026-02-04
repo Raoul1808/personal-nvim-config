b3fb30c3d708d1c9c1a157eeb8969540cde3bb8f
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-02-04 15:54:23 +0100

    Restart

--- init.lua
+++
@@ -1,6 +0,0 @@
-require("config.lazy")
-
-local config_dir = vim.fn.stdpath("config")
----@cast config_dir string
-
-vim.cmd("source" .. vim.fs.joinpath(config_dir, "vim_conf/options.vim"))
--- lua/plugins/dap.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{
-		"mfussenegger/nvim-dap",
-		config = function()
-			require("config.dap")
-		end,
-	},
-}
