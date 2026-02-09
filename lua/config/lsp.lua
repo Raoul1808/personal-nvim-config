f4f7f82c353d0333adf68797d90d3c9723150f06
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-02-09 15:24:54 +0100

    Add diagnostics signs

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,4 +5,16 @@ require("mason-lspconfig").setup({
     },
 })
 
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "󰋼",
+            [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+    },
+})
+
 vim.lsp.enable("rust_analyzer")
