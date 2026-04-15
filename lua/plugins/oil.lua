66420b0831f984d319650061d1844dd3ea965072
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-04-15 19:17:24 +0200

    Add oil.nvim + starter config

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+local gh = require("util").gh
+
+vim.pack.add({
+	gh("nvim-mini/mini.icons"),
+	gh("stevearc/oil.nvim"),
+})
+
+require("oil").setup()
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/util.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+function M.gh(x) return "https://github.com/" .. x end
+function M.cb(x) return "https://codeberg.org/" .. x end
+
+return M
