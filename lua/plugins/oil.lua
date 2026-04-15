6fd0b61c699bc518f237a729052c08b0afee5137
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-04-15 19:32:25 +0200

    Set up mini.icons

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,7 @@ vim.pack.add({
     gh("stevearc/oil.nvim"),
 })
 
+require("mini.icons").setup()
 require("oil").setup()
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
