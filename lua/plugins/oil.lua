c97edffcca4043726fef5923333d3042318b5874
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-04-16 09:56:41 +0200

    Silent install

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,7 @@ local gh = require("util").gh
 vim.pack.add({
     gh("nvim-mini/mini.icons"),
     gh("stevearc/oil.nvim"),
-})
+}, { confirm = false })
 
 require("mini.icons").setup()
 require("oil").setup()
