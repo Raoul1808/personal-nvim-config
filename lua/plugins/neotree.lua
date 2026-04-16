036b4fbe1b9e0f45bf097348bb09a19127ca2981
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2026-04-16 14:22:13 +0200

    neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,4 +6,37 @@ vim.pack.add({
     { src = gh("nvim-neo-tree/neo-tree.nvim"), version = vim.version.range('3') },
 }, {confirm = false})
 
-require("neo-tree").setup({})
+require("neo-tree").setup({
+    source_selector = {
+        winbar = true,
+        sources = {
+            {
+                source = "filesystem",
+                display_name = " 󰉓 Files ",
+            },
+            {
+                source = "buffers",
+                display_name = " 󰈚 Buffers ",
+            },
+            {
+                source = "git_status",
+                display_name = " 󰊢 Git ",
+            },
+            {
+                source = "document_symbols",
+                display_name = "  Symbols "
+            },
+        },
+    },
+    default_component_configs = {
+        indent = {
+            with_expanders = true,
+        },
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+        },
+    },
+})
